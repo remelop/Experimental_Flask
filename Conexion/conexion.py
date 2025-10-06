99789c6108e8c76fb391aad0e915dad424d137ac
Ronny Melo <159053867+remelop@users.noreply.github.com>
2025-10-05 19:01:32 -0500

    Add files via upload

---
+++ Conexion/conexion.py
@@ -0,0 +1,6 @@
+# Importamos la clase MySQL desde la librería Flask-MySQLdb
+from flask_mysqldb import MySQL
+
+# Creamos una instancia de la clase MySQL
+# La inicializaremos más tarde en app.py con la configuración
+mysql = MySQL()
